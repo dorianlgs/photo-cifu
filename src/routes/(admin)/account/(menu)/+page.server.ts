a5df470c27e5c341a16bf6262c57c8062f22a20b
Dorian González <dorianlgs@gmail.com>
2025-01-25 16:16:41 -0500

    Update +page.server.ts

--- src/routes/(admin)/account/(menu)/+page.server.ts
+++ src/routes/(admin)/account/(menu)/+page.server.ts
@@ -1,10 +1,17 @@
 import { redirect } from '@sveltejs/kit';
-import type { Actions } from './$types';
 
-export const actions: Actions = {
-  default: async ({ locals }) => {
-    locals.pb.authStore.clear();
-    locals.user = null;
-    redirect(303, '/');
+export const load = async ({ locals }) => {
+
+  if (!locals.pb.authStore.isValid) {
+    throw redirect(303, '/login');
   }
+
+  // GET USERS
+  const users = await locals.pb.collection('users').getFullList({
+    sort: '-created'
+  });
+
+  console.log({ users })
+
+  return { users: users };
 };
