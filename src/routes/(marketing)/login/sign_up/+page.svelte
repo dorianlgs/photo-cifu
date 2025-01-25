a46f9ebbda85a15757b28ce2a95afad9020aa28d
Dorian González <dorianlgs@gmail.com>
2025-01-25 15:46:07 -0500

    initial commit

---
+++ src/routes/(marketing)/login/sign_up/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Sign up</title>
+</svelte:head>
+
+<h1 class="text-2xl font-bold mb-6">Sign Up</h1>
+
+<div class="text-l text-slate-800 mt-4 mb-2">
+	Have an account? <a class="underline" href="/login/sign_in">Sign in</a>.
+</div>
