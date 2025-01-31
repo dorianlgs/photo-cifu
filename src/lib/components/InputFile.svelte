0edf06765b83b80566a0695a90bd577731286b9a
Dorian González <dorianlgs@gmail.com>
2025-01-30 22:35:02 -0500

    sign up 1

---
+++ src/lib/components/InputFile.svelte
@@ -0,0 +1,45 @@
+<script>
+	let avatarInput = $state();
+
+	let { errors } = $props();
+</script>
+
+<input
+	id={'avatar'}
+	name={'avatar'}
+	type={'file'}
+	bind:this={avatarInput}
+	placeholder={'Upload an avatar'}
+	class="{errors['avatar'] ? 'input-error' : ''} input-md"
+/>
+{#if avatarInput}
+	<h2>Selected files:</h2>
+	{#each Array.from(avatarInput) as file}
+		<p>{file.name} ({file.size} bytes)</p>
+	{/each}
+{/if}
+
+<style>
+	/* file upload button */
+	input[type='file']::file-selector-button {
+		border-radius: 4px;
+		padding: 0 16px;
+		height: 40px;
+		cursor: pointer;
+		background-color: white;
+		border: 1px solid rgba(0, 0, 0, 0.16);
+		box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.05);
+		margin-right: 16px;
+		transition: background-color 200ms;
+	}
+
+	/* file upload button hover state */
+	input[type='file']::file-selector-button:hover {
+		background-color: #f3f4f6;
+	}
+
+	/* file upload button active state */
+	input[type='file']::file-selector-button:active {
+		background-color: #e5e7eb;
+	}
+</style>
--- src/routes/(marketing)/login/sign_in/+page.svelte
+++ src/routes/(marketing)/login/sign_in/+page.svelte
@@ -3,6 +3,7 @@
 	import { pb } from '$lib/pocketbase';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import GitHubButton from '$lib/components/GitHubButton.svelte';
 	import { onMount } from 'svelte';
 
 	let errors: { [fieldName: string]: string } = $state({});
@@ -72,30 +73,25 @@
 		<span>Email verified! Please sign in.</span>
 	</div>
 {/if}
+{#if page.url.searchParams.get('not_verified') === 'true'}
+	<div role="alert" class="alert alert-warning mb-5">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="stroke-current shrink-0 h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			><path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+			/></svg
+		>
+		<span>Please verify your email.</span>
+	</div>
+{/if}
 <h1 class="text-2xl font-bold mb-6">Sign In</h1>
-<button
-	aria-label="Sign in with Github"
-	class="btn btn-github {loading ? 'btn-disabled' : ''}"
-	onclick={async () => {
-		try {
-			await pb.collection('users').authWithOAuth2({ provider: 'github' });
-			goto('/account');
-		} catch (err) {}
-	}}
->
-	<svg
-		fill="gray"
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 30 30"
-		width="21px"
-		height="21px"
-		class="svelte-10a6av0"
-	>
-		<path
-			d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"
-		></path>
-	</svg>
-</button>
+<GitHubButton />
 <br />
 <hr class="solid" />
 <br />
@@ -123,7 +119,7 @@
 	</label>
 	<label for={'password'}>
 		<div class="flex flex-row">
-			<div class="text-base font-bold">{'Your password'}</div>
+			<div class="text-base font-bold">{'Password'}</div>
 			{#if errors['password']}
 				<div class="text-red-600 flex-grow text-sm ml-2 text-right">
 					{errors['password']}
@@ -159,10 +155,3 @@
 <div class="text-l text-slate-800 mt-3">
 	Don't have an account? <a class="underline" href="/login/sign_up">Sign up</a>.
 </div>
-
-<style>
-	.btn-github {
-		background-color: white;
-		border-color: rgb(224, 224, 224);
-	}
-</style>
