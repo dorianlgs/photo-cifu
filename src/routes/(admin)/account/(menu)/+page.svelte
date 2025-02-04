febbcbcdbbbce4fdd1da11fd8a7cec83cced4874
Dorian González <dorianlgs@gmail.com>
2025-02-04 15:53:04 -0500

    gallery 1

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -1,87 +1,91 @@
 <script lang="ts">
+	import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 	import { currentUser } from '$lib/stores/user';
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 	import type { Writable } from 'svelte/store';
+	import { pb } from '$lib/pocketbase';
+	import type { RecordModel } from 'pocketbase';
 
 	let adminSection: Writable<string> = getContext('adminSection');
 	adminSection.set('home');
+
+	let galleries: RecordModel[] = $state([]);
+
+	onMount(async () => {
+		const _galleries = await pb.collection('galleries').getFullList({
+			sort: '-created'
+		});
+
+		galleries = _galleries;
+	});
 </script>
 
 <svelte:head>
 	<title>Account</title>
 </svelte:head>
 
-<h1 class="text-2xl font-bold mb-1">Dashboard - {$currentUser?.name}</h1>
-<div class="alert alert-error max-w-lg mt-2">
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		class="stroke-current shrink-0 h-6 w-6"
-		fill="none"
-		viewBox="0 0 24 24"
-		><path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-		/></svg
-	>
-	<div>
-		<div class="font-bold">Demo Content</div>
-		<div class="my-2">
-			This page is just a placeholder. Replace this page with your app's content and functionality.
-		</div>
-		<div class="my-2">
-			The <a href="/account/billing" class="link">billing</a> and
-			<a href="/account/settings" class="link">settings</a> pages are functional demos.
-		</div>
-	</div>
-</div>
+<h1 class="text-2xl font-bold mb-1">Client Galleries - {$currentUser?.name}</h1>
 
-<div class="my-6">
-	<h1 class="text-xl font-bold mb-1">Users</h1>
-	<div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-		<div class="stat place-items-center">
-			<div class="stat-title">Downloads</div>
-			<div class="stat-value">31K</div>
-			<div class="stat-desc">↗︎ 546 (2%)</div>
-		</div>
-
-		<div class="stat place-items-center">
-			<div class="stat-title">Users</div>
-			<div class="stat-value text-secondary">4,200</div>
-			<div class="stat-desc">↗︎ 40 (2%)</div>
-		</div>
-	</div>
-</div>
-<div class="my-6">
-	<h1 class="text-xl font-bold mb-1">Accounts</h1>
-	<div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-		<div class="stat place-items-center">
-			<div class="stat-title">New Registers</div>
-			<div class="stat-value">1,200</div>
-			<div class="stat-desc">↘︎ 90 (14%)</div>
-		</div>
-
-		<div class="stat place-items-center">
-			<div class="stat-title">Churned Accounts</div>
-			<div class="stat-value">42</div>
-			<div class="stat-desc">↘︎ 6 (12%)</div>
-		</div>
-	</div>
-</div>
-<div class="my-6">
-	<h1 class="text-xl font-bold mb-1">Revenue</h1>
-	<div class="stats shadow stats-vertical sm:stats-horizontal sm:w-[420px]">
-		<div class="stat place-items-center">
-			<div class="stat-title text-success">Revenue</div>
-			<div class="stat-value text-success">$4200</div>
-			<div class="stat-desc">↗︎ $180 (4%)</div>
-		</div>
-
-		<div class="stat place-items-center">
-			<div class="stat-title">New Subscribers</div>
-			<div class="stat-value">16</div>
-			<div class="stat-desc">↘︎ 1 (%7)</div>
-		</div>
-	</div>
+<div class="overflow-x-auto">
+	<table class="table">
+		<thead>
+			<tr>
+				<th>
+					<label>
+						<input type="checkbox" class="checkbox" />
+					</label>
+				</th>
+				<th>Name</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each galleries as gallery}
+				<tr>
+					<th>
+						<label>
+							<input type="checkbox" class="checkbox" />
+						</label>
+					</th>
+					<td>
+						<div class="flex items-center gap-3">
+							<div class="avatar">
+								<div class="mask mask-squircle h-12 w-12">
+									<img
+										src="{PUBLIC_POCKETBASE_URL}/api/files/{gallery.collectionId}/{gallery.id}/{gallery.thumbnail}"
+										alt="Avatar Tailwind CSS Component"
+									/>
+								</div>
+							</div>
+							<div>
+								<div class="font-bold">{gallery.name}</div>
+								<div class="text-sm opacity-50">{gallery.location}</div>
+							</div>
+						</div>
+					</td>
+					<td>
+						Zemlak, Daniel and Leannon
+						<br />
+						<span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+					</td>
+					<td>Purple</td>
+					<th>
+						<a href="/account/gallery/{gallery.id}" class="btn btn-ghost btn-xs">details</a>
+					</th>
+				</tr>
+			{/each}
+		</tbody>
+		<!-- foot -->
+		<tfoot>
+			<tr>
+				<th></th>
+				<th>Name</th>
+				<th>Job</th>
+				<th>Favorite Color</th>
+				<th></th>
+			</tr>
+		</tfoot>
+	</table>
 </div>
---
+++ src/routes/(admin)/account/(menu)/gallery/[slug]/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+	import { pb } from '$lib/pocketbase';
+	import type { RecordModel } from 'pocketbase';
+	import { onMount } from 'svelte';
+
+	import { page } from '$app/state';
+
+	let { slug: galleryId } = page.params;
+
+	let gallery: RecordModel | undefined = $state();
+	let images: string[] = $state([]);
+
+	onMount(async () => {
+		const _gallery = await pb.collection('galleries').getOne(galleryId, { expand: 'images' });
+		gallery = _gallery;
+		images = gallery?.expand?.images;
+	});
+</script>
+
+<svelte:head>
+	<title>{gallery ? gallery.name : ''}</title>
+</svelte:head>
+
+{#if gallery}
+	<div class="carousel rounded-box">
+		{#each images as item}
+			<div class="carousel-item">
+				<img
+					src="{PUBLIC_POCKETBASE_URL}/api/files/{item.collectionId}/{item.id}/{item.image}"
+					alt="Burger"
+					height="400px"
+					width="450px"
+				/>
+			</div>
+		{/each}
+	</div>
+{/if}
