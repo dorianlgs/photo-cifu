bee7ec12190b5c64e1e16879d68c0ffb4774c90d
Dorian González <dorianlgs@gmail.com>
2025-02-09 17:40:22 -0500

    forgot password impl 1

---
+++ pb_migrations/1739140361_updated_galleries.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3598190544")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "file3277268710",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "thumbnail",
+    "presentable": false,
+    "protected": false,
+    "required": true,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3598190544")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "file3277268710",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [],
+    "name": "thumbnail",
+    "presentable": false,
+    "protected": false,
+    "required": true,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+})
