1e9887e8dd27ff8e0712aa45b8f6c9d4f6435ad0
Dorian González <dorianlgs@gmail.com>
2025-03-04 13:03:56 -0500

    Update config.ts

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,5 +1,5 @@
 export const WebsiteName: string = "Photo Cifu"
-export const WebsiteBaseUrl: string = "https://cifu.dev"
+export const WebsiteBaseUrl: string = "https://home-server.tailadc076.ts.net"
 export const WebsiteDescription: string =
     "Open source, fast, and free to host SaaS template. Built with SvelteKit, Pocketbase, Stripe, Tailwind, DaisyUI, and Sqlite"
 export const CreateProfileStep: boolean = true
